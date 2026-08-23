b18e8cd5d70bcc7a8f7d95a4c6b3e25fca8b4d9f
EscabCodex <jere.el1215@gmail.com>
2026-08-23 16:06:10 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // =====================================================
-// VendeFr\u00edo - Service Worker (v92)
+// VendeFr\u00edo - Service Worker (v93)
 // =====================================================
-const CACHE_NAME = "vendefrio-v92";
+const CACHE_NAME = "vendefrio-v93";
 const ARCHIVOS = [
   "./",
   "./index.html",
